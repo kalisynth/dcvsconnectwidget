2449727a0ffdd60d5286f33f7ef7347984617c8a
kalisynth <cade2002@gmail.com>
2016-04-04 13:15:19 +1000

    First Commit

---
+++ app/src/main/java/org/dcvs/nac/dcvsconnectwidget/Dcvschat.java
@@ -0,0 +1,13 @@
+package org.dcvs.nac.dcvsconnectwidget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Dcvschat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dcvschat);
+    }
+}
---
+++ app/src/main/java/org/dcvs/nac/dcvsconnectwidget/Dcvshelp.java
@@ -0,0 +1,13 @@
+package org.dcvs.nac.dcvsconnectwidget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Dcvshelp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dcvshelp);
+    }
+}
