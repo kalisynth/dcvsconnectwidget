aca1933623ff5f93603c51615c4ff500acc5c9e5
kalisynth <cade2002@gmail.com>
2016-04-18 15:48:31 +1000

    added confirm button to call

--- app/src/main/java/org/dcvs/nac/dcvsconnectwidget/Dcvschat.java
+++ app/src/main/java/org/dcvs/nac/dcvsconnectwidget/Dcvschat.java
@@ -2,10 +2,12 @@ package org.dcvs.nac.dcvsconnectwidget;
 
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -53,9 +55,9 @@ public class Dcvschat extends AppCompatActivity {
         finish();
     }
 
-    public void skypedcvs1call(Context skypeCalldcvsv1, String mySkypeDCVSv1){
-        if (!isSkypeClientInstalled(skypeCalldcvsv1)) {
-            goToMarket(skypeCalldcvsv1);
+    public void skypedcvs1call(String mySkypeDCVSv1){
+        if (!isSkypeClientInstalled(this)) {
+            goToMarket(this);
             return;
         }
 
@@ -65,24 +67,104 @@ public class Dcvschat extends AppCompatActivity {
         skypedcvs1Intent.setComponent(new ComponentName("com.skype.raider", "com.skype.raider.Main"));
         skypedcvs1Intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
-        skypeCalldcvsv1.startActivity(skypedcvs1Intent);
+        this.startActivity(skypedcvs1Intent);
 
     }
 
     public void skypev1onclick(View v){
-        skypedcvs1call(this, "skype:volunteer1dcvs?call&video=true");
-    }
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setMessage("Are you sure you want to call Volunteer One?");
+        builder1.setCancelable(true);
+
+        builder1.setPositiveButton(
+                "Yes",
+                new DialogInterface.OnClickListener(){
+                    public void onClick(DialogInterface dialog, int id){
+                        skypedcvs1call("skype:volunteer1dcvs?call&video=true");
+                    }
+                });
+
+        builder1.setNegativeButton(
+                "No",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert1 = builder1.create();
+        alert1.show();
+        }
 
     public void skypev2onclick(View v){
-        skypedcvs1call(this, "skype:volunteer2dcvs?call&video=true");
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setMessage("Are you sure you want to call Volunteer Two?");
+        builder1.setCancelable(true);
+
+        builder1.setPositiveButton(
+                "Yes",
+                new DialogInterface.OnClickListener(){
+                    public void onClick(DialogInterface dialog, int id){
+                        skypedcvs1call("skype:volunteer2dcvs?call&video=true");
+                    }
+                });
+
+        builder1.setNegativeButton(
+                "No",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert1 = builder1.create();
+        alert1.show();
     }
 
     public void skypeofficeonclick(View v){
-        skypedcvs1call(this, "skype:dcvsoffice?call&video=true");
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setMessage("Are you sure you want to call DCVS Office?");
+        builder1.setCancelable(true);
+
+        builder1.setPositiveButton(
+                "Yes",
+                new DialogInterface.OnClickListener(){
+                    public void onClick(DialogInterface dialog, int id){
+                        skypedcvs1call("skype:dcvsoffice?call&video=true");
+                    }
+                });
+
+        builder1.setNegativeButton(
+                "No",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert1 = builder1.create();
+        alert1.show();
     }
 
     public void skypehelpdeskonclick(View v){
-        skypedcvs1call(this, "skype:helpdeskdcvs?call&video=true");
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setMessage("Are you sure you want to call DCVS Help Desk");
+        builder1.setCancelable(true);
+
+        builder1.setPositiveButton(
+                "Yes",
+                new DialogInterface.OnClickListener(){
+                    public void onClick(DialogInterface dialog, int id){
+                        skypedcvs1call("skype:helpdeskdcvs?call&video=true");
+                    }
+                });
+
+        builder1.setNegativeButton(
+                "No",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert1 = builder1.create();
+        alert1.show();
     }
 
 
