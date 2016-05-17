174ff8a85ef2571d2037d822e92f52d3d3fb4a6e
kalisynth <cade2002@gmail.com>
2016-05-17 15:06:43 +1000

    2day

--- app/src/main/java/org/dcvs/nac/dcvsconnectwidget/Dcvschat.java
+++ app/src/main/java/org/dcvs/nac/dcvsconnectwidget/Dcvschat.java
@@ -1,7 +1,9 @@
 package org.dcvs.nac.dcvsconnectwidget;
 
+import android.app.AlertDialog;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
